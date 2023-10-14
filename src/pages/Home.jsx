dbb1b4ebfe414cd0677204df2f3e633cc74fe4b0
Ganesh <ch.tejamr@gmail.com>
2023-10-14 17:44:19 +0530

    working on Roadmap

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from "react"
 import NavBar from "../components/navbar/NavBar"
 import Hero from "../components/hero/Hero"
-// import RoadMap from "../components/roadmap/RoadMap";
+import RoadMap from "../components/roadmap/RoadMap";
 const Home = () => {
   return (
     <div className="bg-[black] min-h-screen">
       <NavBar />
       <Hero />
-      {/* <RoadMap /> */}
+      <RoadMap />
     </div>
   )
 };
